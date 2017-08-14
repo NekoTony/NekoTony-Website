071cb805d3f0444a7ec26b590ec28feb62cbfa8f
NekoTony <cheesezwheel@outlook.com>
2017-08-14 19:45:10 -0400

    Newest Updates

--- avatar.py
+++ avatar.py
@@ -1,32 +1,24 @@
-import discord
 import asyncio
 from flask import Flask, render_template
 import aiohttp
 import time
-from multiprocessing import Process, Value
-from PIL import Image, ImageDraw, ImageFont, ImageColor, ImageOps
 from datetime import datetime
 
-avatar_data = []
-id = "180339602798804992"
-url = 'https://discordapp.com/api/v7/users/{}'.format(id)
-loop = asyncio.get_event_loop()
-session = aiohttp.ClientSession(loop=loop)
-token = ""
-
-
-async def avatarz(token):
-    headers = {}
-    headers["Authorization"] = token
+#Functions
+async def avatarz():
+    headers = {
+    "Authorization" : "BotToken"
+    }
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as resp:
             data = await resp.json()
             try:
-                avatar_data = ["{}".format(data["avatar"])]
+                avatar_data = "{}".format(data["avatar"])
             except:
                 print("Error getting avatar..")
 
-    avatarlink = avatar_url(id, avatar_data[0])
+    avatarlink = avatar_url(id, avatar_data)
+    print(avatarlink)
     async with aiohttp.ClientSession() as ses, ses.get(avatarlink) as r:
         ava = await r.read()
 
@@ -34,15 +26,21 @@ async def avatarz(token):
         print("Saved avatar!!")
         f.write(ava)
 
-
 def avatar_url(id, avatar):
     url = 'https://cdn.discordapp.com/avatars/{}/{}.png?size=1024'.format(
         id, avatar)
     return url
 
+#Variables
+avatar_data = ""
+id = "180339602798804992"
+url = 'https://discordapp.com/api/v7/users/{}'.format(id)
+loop = asyncio.get_event_loop()
+session = aiohttp.ClientSession(loop=loop)
 
 if __name__ == '__main__':
     while True:
-        avatar = loop.run_until_complete(avatarz(token))
+        avatar = loop.run_until_complete(avatarz())
         print(str(datetime.now()))
         time.sleep(3600)
+
--- main.py
+++ main.py
@@ -1,60 +1,39 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-import discord
 import asyncio
 from flask import Flask, render_template, url_for
 import aiohttp
 import os
 import time
 from htmlmin.minify import html_minify
-from multiprocessing import Process, Value
-from PIL import Image, ImageDraw, ImageFont, ImageColor, ImageOps
+from datetime import datetime, date
 
 app = Flask(__name__)
-loop = asyncio.get_event_loop()
-session = aiohttp.ClientSession(loop=loop)
 
 
-async def discord_name():
-    global username
-    token = ""
-    url = 'https://discordapp.com/api/v7/users/180339602798804992'
-    headers = {}
-    headers["Authorization"] = token
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
-            try:
-                username = data["username"]
-            except:
-                username = "NekoTony"
-
-            return username
-
+#Flask Functions
 @app.route('/')
 def home():
     title = "{} | Home".format(username.upper())
     video = "True"
-    return html_minify(render_template('index.html', title=title, video=video))
+    return html_minify(render_template('index.html', title=title, video=video, desc=desc))
 
 @app.route('/about')
 def about():
     title = "{} | About".format(username.upper())
     video = "False"
-    return html_minify(render_template('about.html', title=title, video=video))
-
+    age = calculate_age(datetime.strptime('Aug 27 2000', '%b %d %Y'))
+    return html_minify(render_template('about.html', title=title, video=video, desc=desc))
 
 @app.route('/portfolio')
 def portfolio():
     title = "{} | Portfolio".format(username.upper())
     video = "False"
-    return html_minify(render_template('portfolio.html', title=title, video=video))
+    return html_minify(render_template('portfolio.html', title=title, video=video, desc=desc))
 
 @app.route('/contact')
 def contact():
     title = "{} | Contact".format(username.upper())
     video = "False"
-    return html_minify(render_template('contact.html', title=title, video=video))
+    return html_minify(render_template('contact.html', title=title, video=video, desc=desc))
 
 
 @app.errorhandler(404)
@@ -66,7 +45,7 @@ def page_not_found(e):
 def override_url_for():
     return dict(url_for=dated_url_for)
 
-
+#Regular Functions
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
         filename = values.get('filename', None)
@@ -76,7 +55,34 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day)) #borrowed from stack overflow. Coudln't find a module to get age from date.
+
+async def discord_name():
+    global username
+    url = 'https://discordapp.com/api/v7/users/180339602798804992'
+    headers = {
+    "Authorization" : "BotToken"
+    }
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url) as resp:
+            data = await resp.json()
+            try:
+                username = data["username"]
+            except:
+                username = "NekoTony"
+
+            return username
+
+#Main Variables
+username = ""
+loop = asyncio.get_event_loop()
+session = aiohttp.ClientSession(loop=loop)
+username = loop.run_until_complete(discord_name())
+desc = "Hello, I'm {} and welcome to http://NekoTony.pro/. This is my personal website, made by me. Enjoy! ".format(username)
+
 
 if __name__ == '__main__':
-    username = loop.run_until_complete(discord_name())
     app.run(debug=True, host='0.0.0.0', port=9000)
+
